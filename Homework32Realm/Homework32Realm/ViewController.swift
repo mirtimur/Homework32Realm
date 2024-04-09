cabdb6ef6c98f27a6784ae1e6b7ba8349ff24326
Timur Mir <mirtimur1@gmail.com>
2024-04-09 18:30:44 +0300

    Initial commit

---
+++ Homework32Realm/Homework32Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework32Realm
+//
+//  Created by Timur Mir on 9.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
